bd3f452cba6b8fdc3c24caac806c6a0252773bbf
nesrindemir <nnessrrinn85@hotmail.com>
2022-12-20 10:19:13 +0300

    final commit

---
+++ src/hesapMakinesi.java
@@ -0,0 +1,207 @@
+import java.util.Scanner;
+public class hesapMakinesi {
+    static void plus() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 0, i = 1;
+        while (true) {
+            System.out.print(i++ + ". sayı :");
+            number = scan.nextInt();
+            if (number == 0) {
+                break;
+            }
+            result += number;
+        }
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void minus() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        int number, result = 0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextInt();
+            if (i == 1) {
+                result += number;
+                continue;
+            }
+            result -= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void times() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 1, i = 1;
+
+        while (true) {
+            System.out.print(i++ + ". sayı :");
+            number = scan.nextInt();
+
+            if (number == 1)
+                break;
+
+            if (number == 0) {
+                result = 0;
+                break;
+            }
+            result *= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void divided() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        double number, result = 0.0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextDouble();
+            if (i != 1 && number == 0) {
+                System.out.println("Böleni 0 giremezsiniz.");
+                continue;
+            }
+            if (i == 1) {
+                result = number;
+                continue;
+            }
+            result /= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void power() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değeri giriniz :");
+        int base = scan.nextInt();
+        System.out.print("Üs değeri giriniz :");
+        int exponent = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void factorial() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı giriniz :");
+        int n = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static boolean modAlma(){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Birinci sayıyı giriniz:");
+        int n1=scan.nextInt();
+        System.out.print("ikinci sayıyı giriniz :");
+        int n2=scan.nextInt();
+        int a,b; //a büyük sayı ,b kücük sayı
+        int result=0;
+        if(n1<n2){
+            a=n2;
+            b=n1;
+        }else{
+            a=n1;
+            b=n2;
+        }
+
+        if(a!=0 && b!=0){
+            result=a%b;
+        }else{
+            System.out.println("Böleni sıfır giremezsiniz!");
+            return false;
+        }
+
+        System.out.println("Sonuç :"+result);
+        return true;
+    }
+
+    static void dikdortgenCevreAlan(){
+        Scanner scan = new Scanner(System.in);
+        int a,b,cevre;
+         int alan;
+        System.out.print("Dikdörtgenin uzun kenarını giriniz :");
+        a=scan.nextInt();
+        System.out.print("Dikdörtgenin kısa kenarını giriniz :");
+        b=scan.nextInt();
+        if(a==0 || b==0){
+            System.out.println("ölçüleri 0 giremezsiniz");
+            return;
+        }else {
+            cevre=2*(a+b);
+            alan=a*b;
+
+        }
+        System.out.println("dikdörtgenin cevresi : "+cevre);
+        System.out.println("dikdörtgenin alanı : "+alan);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int select;
+        String menu = "1- Toplama İşlemi\n"
+                + "2- Çıkarma İşlemi\n"
+                + "3- Çarpma İşlemi\n"
+                + "4- Bölme işlemi\n"
+                + "5- Üslü Sayı Hesaplama\n"
+                + "6- Faktoriyel Hesaplama\n"
+                + "7- Mod Alma\n"
+                + "8- Dikdörtgen Alan ve Çevre Hesabı\n"
+                + "0- Çıkış Yap";
+
+        do {
+            System.out.println(menu);
+            System.out.print("Lütfen bir işlem seçiniz :");
+            select = scan.nextInt();
+            switch (select) {
+                case 1:
+                    plus();
+                    break;
+                case 2:
+                    minus();
+                    break;
+                case 3:
+                    times();
+                    break;
+                case 4:
+                    divided();
+                    break;
+                case 5:
+                    power();
+                    break;
+                case 6:
+                    factorial();
+                    break;
+                case 7:
+                    modAlma();
+                    break;
+                case 8:
+                    dikdortgenCevreAlan();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Yanlış bir değer girdiniz, tekrar deneyiniz.");
+            }
+        } while (select != 0);
+
+
+    }
+}
